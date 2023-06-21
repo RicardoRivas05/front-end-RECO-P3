90fbed967e16c051c0b04d2b3ca76f0092645292
RicardoRivas <ribadariloz2370@gmail.com>
2023-06-21 13:22:31 -0600

    Correcion en las tablas de Valor Maximo y Minimo

--- src/components/SecondTableReport.tsx
+++ src/components/SecondTableReport.tsx
@@ -1,83 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { getDatos } from '../helpers';
+import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 
-interface TableProps{
-    data: {
-        sourceId: string;
-        value: number;
-        dateTime: string
-    }[];
+interface TableProps {
+  data: {
+    sourceId: string;
+    value: number;
+    dateTime: string;
+  }[];
 }
 
+const TableVMin: React.FC<TableProps> = ({ data }) => {
+  const [idNames, setIdNames] = useState<{ [key: string]: string }>({});
+  const [minFech, setMinFech] = useState<{ [key: string]: Date }>({});
 
+  useEffect(() => {
+    const idNamesResponse: { [key: string]: string } = {
+      "645e79a1ac39284b585fb464": "S1 WIND SPEED SCALED",
+      "645e79a6ac39284b585fb465": "S2 WIND SPEED SCALED",
+      "645e9a7bac39284b585fb469": "S3 WIND SPEED SCALED",
+      "645e9a8eac39284b585fb46a": "S4 WIND SPEED SCALED",
+      "645e9a93ac39284b585fb46b": "S5 WIND SPEED SCALED",
+      "645e9a98ac39284b585fb46c": "S6 WIND SPEED SCALED",
+    };
+    setIdNames(idNamesResponse);
 
- const TableVMin: React.FC<TableProps> = ({data}) => {
-    const [idNames, setIdNames] = useState<{[key:string]: string}>({});
+    const getMinMaxFech = () => {
+      const minFech: { [key: string]: Date } = {};
+      data.forEach((item) => {
+        const { sourceId, value, dateTime } = item;
+        const dateObj = new Date(dateTime);
 
-    useEffect(() => {
+        if (!minFech[sourceId] || value < minFech[sourceId].valueOf()) {
+          minFech[sourceId] = dateObj;
+        }
+      });
+      return { minFech };
+    };
 
-
-
-        const  idNamesResponse:{[key:string]:string} = {
-            "645e79a1ac39284b585fb464": "S1 WIND SPEED SCALED",
-            "645e79a6ac39284b585fb465": "S2 WIND SPEED SCALED",
-            "645e9a7bac39284b585fb469": "S3 WIND SPEED SCALED",
-            "645e9a8eac39284b585fb46a": "S4 WIND SPEED SCALED",
-            "645e9a93ac39284b585fb46b": "S5 WIND SPEED SCALED",
-            "645e9a98ac39284b585fb46c": "S6 WIND SPEED SCALED",
-        };
-        setIdNames(idNamesResponse);
-    },[]);
-
+    const { minFech } = getMinMaxFech();
+    setMinFech(minFech);
+  }, [data]);
 
   const getMinValues = () => {
-    const minValues: {[key:string]: number} = {};
+    const minValues: { [key: string]: number } = {};
     data.forEach((item) => {
-        const {sourceId, value} = item;
-        if(!minValues[sourceId] || value < minValues[sourceId]){
-            minValues[sourceId] =  value;
-        }
+      const { sourceId, value } = item;
+      if (!minValues[sourceId] || value <= minValues[sourceId]) {
+        minValues[sourceId] = value;
+      }
     });
-    return minValues 
+    return minValues;
   };
 
-  const getMinFech = () => {
-    const minFech: {[key:string]: string} = {};
-    data.forEach((item) => {
-        const {sourceId, value, dateTime} = item;
-        if(!minFech[sourceId] || value < Number(minFech[sourceId])){
-            const formattedDate = dayjs(dateTime).format('YYYY-MM-DD HH:mm:ss');
-            minFech[sourceId] = formattedDate;
-        }
-    });
-    return minFech;
-  }
+  const minValues = getMinValues();
 
-  const minValues = getMinValues ();
-  const minFech = getMinFech();
-
-  return(
-    
-    <table style={{borderCollapse:'collapse', marginBottom:'20px', marginLeft:'5px'}}>
-        <thead>
-            <tr>
-                <th style={{padding:'8px', textAlign:'left', borderBottom:'1px solud #ddd', backgroundColor:'#f2f2f2'}}>Estación</th>
-                <th style={{padding:'8px', textAlign:'left', borderBottom:'1px solud #ddd', backgroundColor:'#f2f2f2'}}>Valor mas Bajo</th>
-                <th style={{padding:'8px', textAlign:'left', borderBottom:'1px solud #ddd', backgroundColor:'#f2f2f2'}}>Fecha</th>
-                
-            </tr>
-        </thead>
-        <tbody>
-            {Object.keys(minValues).map((sourceId)=> 
-                <tr key={sourceId}>
-                    <td style={{padding:'8px', textAlign:'left', borderBottom:'1px solid #ddd'}}>{idNames[sourceId]}</td>
-                    <td style={{padding:'8px', textAlign:'left', borderBottom:'1px solid #ddd'}}>{minValues[sourceId]}</td>
-                    <td style={{padding:'8px', textAlign:'left', borderBottom:'1px solid #ddd'}}>{minFech[sourceId]}</td>
-                    
-                </tr>
-            )}
-        </tbody>
+  return (
+    <table style={{ borderCollapse: 'collapse', marginBottom: '20px', marginLeft: '5px' }}>
+      <thead>
+        <tr>
+          <th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>Estación</th>
+          <th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>Valor más Bajo</th>
+          <th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>Fecha</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(minValues).map((sourceId) =>
+          <tr key={sourceId}>
+            <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{idNames[sourceId]}</td>
+            <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{minValues[sourceId]}</td>
+            <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{minFech[sourceId]?.toLocaleString()}</td>
+          </tr>
+        )}
+      </tbody>
     </table>
   );
 };
--- src/components/maxValue.tsx
+++ src/components/maxValue.tsx
@@ -1,77 +1,77 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 
-
-interface TableProps{
-    data: {
-        sourceId: string;
-        value: number;
-        dateTime: string;
-    }[];
+interface TableProps {
+  data: {
+    sourceId: string;
+    value: number;
+    dateTime: string;
+  }[];
 }
- const TableVMax: React.FC<TableProps> = ({data}) => {
-    const [idNames, setIdNames] = useState<{[key:string]: string}>({});
 
-    useEffect(() => {
+const TableVMax: React.FC<TableProps> = ({ data }) => {
+  const [idNames, setIdNames] = useState<{ [key: string]: string }>({});
+  const [maxFech, setMaxFech] = useState<{ [key: string]: Date }>({});
 
+  useEffect(() => {
+    const idNamesResponse: { [key: string]: string } = {
+      "645e79a1ac39284b585fb464": "S1 WIND SPEED SCALED",
+      "645e79a6ac39284b585fb465": "S2 WIND SPEED SCALED",
+      "645e9a7bac39284b585fb469": "S3 WIND SPEED SCALED",
+      "645e9a8eac39284b585fb46a": "S4 WIND SPEED SCALED",
+      "645e9a93ac39284b585fb46b": "S5 WIND SPEED SCALED",
+      "645e9a98ac39284b585fb46c": "S6 WIND SPEED SCALED",
+    };
+    setIdNames(idNamesResponse);
 
+    const getMinMaxFech = () => {
+      const maxFech: { [key: string]: Date } = {};
+      data.forEach((item) => {
+        const { sourceId, value, dateTime } = item;
+        const dateObj = new Date(dateTime);
 
-        const  idNamesResponse:{[key:string]:string} = {
-            "645e79a1ac39284b585fb464": "S1 WIND SPEED SCALED",
-            "645e79a6ac39284b585fb465": "S2 WIND SPEED SCALED",
-            "645e9a7bac39284b585fb469": "S3 WIND SPEED SCALED",
-            "645e9a8eac39284b585fb46a": "S4 WIND SPEED SCALED",
-            "645e9a93ac39284b585fb46b": "S5 WIND SPEED SCALED",
-            "645e9a98ac39284b585fb46c": "S6 WIND SPEED SCALED",
-        };
-        setIdNames(idNamesResponse);
-    },[]);
+        if (!maxFech[sourceId] || value > maxFech[sourceId].valueOf()) {
+          maxFech[sourceId] = dateObj;
+        }
+      });
+      return { maxFech };
+    };
 
+    const { maxFech } = getMinMaxFech();
+    setMaxFech(maxFech);
+  }, [data]);
 
   const getMaxValues = () => {
-    const maxValues: {[key:string]: number} = {};
+    const maxValues: { [key: string]: number } = {};
     data.forEach((item) => {
-        const {sourceId, value} = item;
-        if(!maxValues[sourceId] || value > maxValues[sourceId]){
-            maxValues[sourceId] = value;
-        }
+      const { sourceId, value } = item;
+      if (!maxValues[sourceId] || value > maxValues[sourceId]) {
+        maxValues[sourceId] = value;
+      }
     });
-    return maxValues
+    return maxValues;
   };
 
-  const getMaxFech = () => {
-    const maxFech: {[key:string]: string} = {};
-    data.forEach((item) => {
-        const {sourceId, value, dateTime} = item;
-        if(!maxFech[sourceId] || value > Number(maxFech[sourceId])){
-            const formattedDate = dayjs(dateTime).format('YYYY-MM-DD HH:mm:ss');
-            maxFech[sourceId] = formattedDate;
-        }
-    }); 
-    return maxFech;
-  }
+  const maxValues = getMaxValues();
 
-  const maxValues = getMaxValues ();
-  const maxFech = getMaxFech();
-
-  return(
-    <table style={{borderCollapse:'collapse', marginBottom:'20px', marginLeft:'2px'}}>
-        <thead>
-            <tr>
-                <th style={{padding:'8px', textAlign:'left', borderBottom:'1px solud #ddd', backgroundColor:'#f2f2f2'}}>Estación</th>
-                <th style={{padding:'8px', textAlign:'left', borderBottom:'1px solud #ddd', backgroundColor:'#f2f2f2'}}>Valor mas Alto</th>
-                <th style={{padding:'8px', textAlign:'left', borderBottom:'1px solud #ddd', backgroundColor:'#f2f2f2'}}>Fecha</th>
-            </tr>
-        </thead>
-        <tbody>
-            {Object.keys(maxValues).map((sourceId)=> 
-                <tr key={sourceId}>
-                    <td style={{padding:'8px', textAlign:'left', borderBottom:'1px solid #ddd'}}>{idNames[sourceId]}</td>
-                    <td style={{padding:'8px', textAlign:'left', borderBottom:'1px solid #ddd'}}>{maxValues[sourceId]}</td>
-                    <td style={{padding:'8px', textAlign:'left', borderBottom:'1px solid #ddd'}}>{maxFech[sourceId]}</td>
-                </tr>
-            )}
-        </tbody>
+  return (
+    <table style={{ borderCollapse: 'collapse', marginBottom: '20px', marginLeft: '5px' }}>
+      <thead>
+        <tr>
+          <th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>Estación</th>
+          <th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>Valor más Alto</th>
+          <th style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd', backgroundColor: '#f2f2f2' }}>Fecha</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(maxValues).map((sourceId) =>
+          <tr key={sourceId}>
+            <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{idNames[sourceId]}</td>
+            <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{maxValues[sourceId]}</td>
+            <td style={{ padding: '8px', textAlign: 'left', borderBottom: '1px solid #ddd' }}>{maxFech[sourceId]?.toLocaleString()}</td>
+          </tr>
+        )}
+      </tbody>
     </table>
   );
 };
