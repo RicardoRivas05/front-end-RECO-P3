bdd6ed3788bdc968de609f90a51c8bf5d7311dc2
RicardoRivas <ribadariloz2370@gmail.com>
2023-07-06 14:49:55 -0600

    Funcionalidad Rangos predefinidos

--- src/components/ParametersReport.tsx
+++ src/components/ParametersReport.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
+ import React, { useEffect, useState } from 'react';
 import { Card, Col, Row, DatePicker, Space, Select, Button, message } from 'antd';
 import { FundTwoTone } from '@ant-design/icons';
 import moment from 'moment';
 import { getSource } from '../helpers';
 
 const { RangePicker } = DatePicker;
-
+const { Option } = Select;
+ 
 export const ParametersReport: React.FC = ({ handleParameters }: any) => {
   const [dataSource, setSource] = useState<any>();
   const [fechaInicio, setFechaInicio] = useState<moment.Moment | null>(null);
   const [fechaFin, setFechaFin] = useState<moment.Moment | null>(null);
-  const [estaciones, setEstaciones] = useState<any>('');
+  const [estaciones, setEstaciones] = useState<string[]>([]);
+  const [selectedRange, setSelectedRange] = useState<string>('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [isCustomRangeDisabled, setIsCustomRangeDisabled] = useState(false);
 
-
-  
   const HandleSource = async () => {
     const getData = await getSource();
     const list = getData.map((item) => {
       return { label: item.name, value: item.id };
     });
     setSource(list);
-    
   };
-
 
   useEffect(() => {
     HandleSource();
@@ -31,19 +31,80 @@ export const ParametersReport: React.FC = ({ handleParameters }: any) => {
   const onChange = (dates: [moment.Moment | null, moment.Moment | null], dateStrings: [string, string]) => {
     setFechaInicio(dates[0]);
     setFechaFin(dates[1]);
+
+    if (dates[0] && dates[1]) {
+      setIsCustomRangeDisabled(true);
+      setIsButtonDisabled(estaciones.length === 0);
+      setSelectedRange('');
+    } else {
+      setIsCustomRangeDisabled(false);
+      setIsButtonDisabled(selectedRange === '' && estaciones.length === 0);
+    }
+  };
+
+  const handleSelectedRange = (value: string) => {
+    setSelectedRange(value);
+    setIsButtonDisabled(!value && estaciones.length === 0);
+    setFechaInicio(null);
+    setFechaFin(null);
+    setIsCustomRangeDisabled(false);
+  };
+
+  const handleEstacionesChange = (selectedEstaciones: string[]) => {
+    setEstaciones(selectedEstaciones);
+    setIsButtonDisabled(!selectedRange && selectedEstaciones.length === 0);
   };
 
   const handleGenerateReport = () => {
-    if (!fechaInicio || !fechaFin || !estaciones) {
-      message.error('Por favor, complete el rango de fecha, las estaciones y la hora.');
+    if ((!fechaInicio || !fechaFin) && !selectedRange) {
+      message.error('Por favor, complete el rango de fecha.');
       return;
     }
-
-    const fechaInicioString = fechaInicio.format('YYYY-MM-DD');
-    const fechaFinString = fechaFin.format('YYYY-MM-DD');
-
-    handleParameters([fechaInicioString, fechaFinString], estaciones);
+  
+    if (estaciones.length === 0) {
+      message.error('Por favor, seleccione al menos una estación.');
+      return;
+    }
+  
+    if (selectedRange) {
+      const today = moment().startOf('day');
+      let startDate;
+      let endDate;
+  
+      switch (selectedRange) {
+        case '1':
+          startDate = today.subtract(1, 'day');
+          endDate = today;
+          break;
+        case '3':
+          startDate = today.subtract(3, 'day');
+          endDate = today;
+          break;
+        case '7':
+          startDate = today.subtract(1, 'week');
+          endDate = today;
+          break;
+        case '14':
+          startDate = today.subtract(2, 'week');
+          endDate = today;
+          break;
+        case '30':
+          startDate = today.subtract(1, 'month');
+          endDate = today;
+          break;
+        default:
+          startDate = null;
+          endDate = null;
+      }
+  
+      handleParameters([startDate, endDate], estaciones);
+    } else {
+      const fechaInicioString = fechaInicio ? fechaInicio.format('YYYY-MM-DD') : '';
+      const fechaFinString = fechaFin ? fechaFin.format('YYYY-MM-DD') : '';
+      handleParameters([fechaInicioString, fechaFinString], estaciones);
+    }
   };
+  
 
   return (
     <Row>
@@ -59,9 +120,10 @@ export const ParametersReport: React.FC = ({ handleParameters }: any) => {
             <Col span={10}>
               <Space direction="vertical" size={12}>
                 <RangePicker
-                  showTime={{ format: 'HH:mm' }}
+                  showTime={false}
                   format="YYYY-MM-DD"
                   onChange={onChange}
+                  disabled={!!selectedRange}
                 />
               </Space>
             </Col>
@@ -69,9 +131,9 @@ export const ParametersReport: React.FC = ({ handleParameters }: any) => {
               <Select
                 mode="multiple"
                 allowClear
-                style={{ width: '90%' }}
+                style= {{ width: '90%' }}
                 placeholder="Seleccionar estaciones"
-                onChange={setEstaciones}
+                onChange={handleEstacionesChange}
                 options={dataSource}
               />
             </Col>
@@ -82,9 +144,28 @@ export const ParametersReport: React.FC = ({ handleParameters }: any) => {
                 icon={<FundTwoTone />}
                 size="middle"
                 onClick={handleGenerateReport}
+                disabled={isButtonDisabled}
               >
                 Generar Reporte
               </Button>
+            </Col>
+          </Row>
+          <Row style={{ marginTop: '10px' }}>
+            <Col span={10}>
+              <Select
+                style={{ width: '90%' }}
+                placeholder="Seleccionar rango predefinido"
+                onChange={handleSelectedRange}
+                value={selectedRange}
+                disabled={isCustomRangeDisabled}
+              >
+                <Option value="">Seleccionar rango predefinido</Option>
+                <Option value="1">1 día</Option>
+                <Option value="3">3 días</Option>
+                <Option value="7">1 semana</Option>
+                <Option value="14">2 semanas</Option>
+                <Option value="30">1 mes</Option>
+              </Select>
             </Col>
           </Row>
         </Card>
@@ -95,6 +176,163 @@ export const ParametersReport: React.FC = ({ handleParameters }: any) => {
 };
 
 export default ParametersReport;
+
+
+
+// import React, { useEffect, useState } from 'react';
+// import { Card, Col, Row, DatePicker, Space, Select, Button, message } from 'antd';
+// import { FundTwoTone } from '@ant-design/icons';
+// import moment from 'moment';
+// import { getSource } from '../helpers';
+
+// const { RangePicker } = DatePicker;
+// const { Option } = Select;
+
+// export const ParametersReport: React.FC = ({ handleParameters }: any) => {
+//   const [dataSource, setSource] = useState<any>();
+//   const [fechaInicio, setFechaInicio] = useState<moment.Moment | null>(null);
+//   const [fechaFin, setFechaFin] = useState<moment.Moment | null>(null);
+//   const [estaciones, setEstaciones] = useState<string[]>([]);
+//   const [selectedRange, setSelectedRange] = useState<string>('');
+//   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+//   const HandleSource = async () => {
+//     const getData = await getSource();
+//     const list = getData.map((item) => {
+//       return { label: item.name, value: item.id };
+//     });
+//     setSource(list);
+//   };
+
+//   useEffect(() => {
+//     HandleSource();
+//   }, []);
+
+//   // ...
+//   const onChange = (dates: [moment.Moment | null, moment.Moment | null], dateStrings: [string, string]) => {
+//     setFechaInicio(dates[0]);
+//     setFechaFin(dates[1]);
+//     setIsButtonDisabled(!isRangeSelected(selectedRange) && (!dates[0] || !dates[1]) || estaciones.length === 0);
+//   };
+  
+//   const handleSelectedRange = (value: string) => {
+//     setSelectedRange(value);
+//     setIsButtonDisabled((!isRangeSelected(value) || !isRangeSelected([fechaInicio, fechaFin])) && estaciones.length === 0);
+//     if (value) {
+//       setFechaInicio(null);
+//       setFechaFin(null);
+//     }
+//   };
+  
+//   const handleEstacionesChange = (selectedEstaciones: string[]) => {
+//     setEstaciones(selectedEstaciones);
+//     setIsButtonDisabled((!isRangeSelected(selectedRange) || !isRangeSelected([fechaInicio, fechaFin])) && selectedEstaciones.length === 0);
+//   };
+
+//   // ...
+//   const isRangeSelected = (range: string | [moment.Moment | null, moment.Moment | null]) => {
+//     if (typeof range === 'string') {
+//       return range !== '';
+//     } else {
+//       return range[0] !== null || range[1] !== null;
+//     }
+//   };
+
+// // ...
+
+//   const handleGenerateReport = () => {
+//     if ((!fechaInicio || !fechaFin) && !selectedRange) {
+//       message.error('Por favor, complete el rango de fecha.');
+//       return;
+//     }
+
+//     if (estaciones.length === 0) {
+//       message.error('Por favor, seleccione al menos una estación.');
+//       return;
+//     }
+
+//     if (selectedRange) {
+//       handleParameters([], estaciones); // Pasar un arreglo vacío para indicar que se utilizará el rango predefinido
+//     } else {
+//       const fechaInicioString = fechaInicio ? fechaInicio.format('YYYY-MM-DD') : '';
+//       const fechaFinString = fechaFin ? fechaFin.format('YYYY-MM-DD') : '';
+//       handleParameters([fechaInicioString, fechaFinString], estaciones);
+//     }
+//   };
+
+//   return (
+//     <Row>
+//       <Col span={1}></Col>
+//       <Col span={22}>
+//         <Card
+//           title="Seleccionar Parámetros"
+//           bordered={true}
+//           hoverable={true}
+//           style={{ border: '1px solid Gainsboro' }}
+//         >
+//           <Row>
+//             <Col span={10}>
+//               <Space direction="vertical" size={12}>
+//                 <RangePicker
+//                   format="YYYY-MM-DD"
+//                   onChange={onChange}
+//                   disabled={!!selectedRange}
+//                   showTime={false}
+//                 />
+//               </Space>
+//             </Col>
+//             <Col span={10}>
+//               <Select
+//                 mode="multiple"
+//                 allowClear
+//                 style={{ width: '90%' }}
+//                 placeholder="Seleccionar estaciones"
+//                 onChange={handleEstacionesChange}
+//                 options={dataSource}
+//               />
+//             </Col>
+//             <Col span={3}>
+//               <Button
+//                 type="primary"
+//                 shape="round"
+//                 icon={<FundTwoTone />}
+//                 size="middle"
+//                 onClick={handleGenerateReport}
+//                 disabled={isButtonDisabled}
+//               >
+//                 Generar Reporte
+//               </Button>
+//             </Col>
+//           </Row>
+//           <Row style={{ marginTop: '10px' }}>
+//             <Col span={10}>
+//               <Select
+//                 style={{ width: '60%' }}
+//                 placeholder="Seleccionar rango predefinido"
+//                 onChange={handleSelectedRange}
+//                 value={selectedRange}
+//               >
+//                 <Option value="">Seleccionar rango predefinido</Option>
+//                 <Option value="1">1 día</Option>
+//                 <Option value="3">3 días</Option>
+//                 <Option value="7">1 semana</Option>
+//                 <Option value="14">2 semanas</Option>
+//                 <Option value="30">1 mes</Option>
+//               </Select>
+//             </Col>
+//           </Row>
+//         </Card>
+//       </Col>
+//       <Col span={1}></Col>
+//     </Row>
+//   );
+// };
+
+// export default ParametersReport;
+
+
+
+
 
 // import React, { useEffect, useState } from 'react';
 // import { Card, Col, Row, DatePicker, Space, Select, Button, message } from 'antd';
@@ -177,84 +415,4 @@ export default ParametersReport;
 //   );
 // };
 
-// export default ParametersReport;
-
-// // import React, { useEffect, useState } from 'react';
-// // import { Card, Col, Row, DatePicker, Space, Select, Button } from 'antd';
-// // import type { DatePickerProps, RangePickerProps } from 'antd/es/date-picker';
-// // import { FundTwoTone } from '@ant-design/icons';
-// // import {  getSource } from '../helpers';
-
-// // const { RangePicker } = DatePicker;
-
-// // export const ParametersReport: React.FC = ({handleParameters}:any) => {
-// //   const [dataSource, setSource] = useState<any>();
-// //   const [fechas,setFechas] = useState<any>([]);
-// //   const [estaciones,setEstaciones] = useState<any>('');
-
-// //   const HandleSource = async () => {
-// //     const getData = await getSource()
-// //     const list = getData.map((item) => {
-// //       return { "label": item.name, "value": item.id }
-// //     })
-// //     setSource(list)
-// //   }
-// //   useEffect(() => {
-// //     HandleSource()
-// //   }, []);
-
-// //   const onChange = (
-// //     value: DatePickerProps['value'] | RangePickerProps['value'],
-// //     dateString: [string, string],
-// //   ) => {
-// //     setFechas(dateString)
-// //   };
-
-// //   const onOk =(value:DatePickerProps['value'] | RangePickerProps['value'])=>{
-// //     console.log('onOk: ', value);
-// //   };
-
-// //   return (
-// //     <Row>
-// //       <Col span={1}></Col>
-// //       <Col span={22}>
-// //         <Card title="Seleccionar Parametros" bordered={true} hoverable={true} style={{ border: '1px solid Gainsboro' }}>
-// //           <Row>
-// //             <Col span={10}>
-// //             <Space direction="vertical" size={12}>
-// //                 <RangePicker
-// //                   // showTime={{ format: 'HH:mm' }}
-// //                   format="YYYY-MM-DD"
-// //                   onChange={onChange}
-// //                   onOk={onOk}
-// //                 />
-// //               </Space>
-// //             </Col>
-// //             <Col span={10}>
-// //               <Select
-// //                 mode="multiple"
-// //                 allowClear
-// //                 style={{ width: '90%' }}
-// //                 placeholder="Seleccionar estaciones "
-// //                 onChange={setEstaciones}
-// //                 options={dataSource}
-// //               />
-// //             </Col>
-// //             <Col span={3}>
-// //               <Button type="primary" shape="round" icon={<FundTwoTone />} size="middle" 
-// //               onClick={()=>{
-// //                 handleParameters(fechas,estaciones)
-// //               }} 
-// //               >
-// //                 Generar Reporte
-// //               </Button>
-// //             </Col>
-// //           </Row>
-// //         </Card>
-// //       </Col>
-// //       <Col span={1}></Col>
-// //     </Row>
-// //   )
-// // };
-
-// // export default ParametersReport;
+// export default ParametersReport
